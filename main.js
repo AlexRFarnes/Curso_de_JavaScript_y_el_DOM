5dd1d568924915a89d563b53960a89f1ab26d18f
Alexander R. Farnes <alexrfarnes@gmail.com>
2025-03-11 13:20:49 +0800

    Start events section

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const title = document.getElementById('title');
 title.innerText = 'Cursos';
 
 const description = document.getElementById('description');
-description.innerText = 'Listado de cursos';
+description.textContent = 'Listado de cursos';
 
 // const items = document.getElementsByClassName('list-group-item');
 const items = document.querySelectorAll('li:nth-child(odd)');
@@ -18,8 +18,41 @@ const list = document.querySelector('ul');
 // console.log(list.children);
 
 for (let i = 0; i < list.children.length; i++) {
-  console.log(list.children[i]);
+  // console.log(list.children[i]);
 }
 
-console.log(list.firstElementChild);
-console.log(list.lastElementChild);
+// console.log(list.firstElementChild);
+// console.log(list.lastElementChild);
+
+// parent
+const element = document.querySelector('li');
+// console.log(element.parentElement);
+
+// sibling
+// console.log(element.nextElementSibling);
+// console.log(element.nextElementSibling.nextElementSibling);
+
+const lastElement = document.querySelector('#last-course');
+// console.log(lastElement.previousElementSibling);
+
+// nodes
+console.log(element.childElementCount); // only count HTML tags
+console.log(element.childNodes);
+
+// attributes
+console.log(description.innerHTML);
+
+// event listeners
+const btn = document.querySelector('.btn');
+// btn.addEventListener('click', e => {
+//   console.log(e.target);
+// });
+
+addGlobalListener('click', btn);
+
+function addGlobalListener(eventType, element) {
+  document.addEventListener(eventType, e => {
+    if (e.target != element) return;
+    console.log(element);
+  });
+}
