31049f56bb2629877c565622cbb097531a1d3e00
Alexander R. Farnes <alexrfarnes@gmail.com>
2025-03-12 13:07:51 +0800

    Complete course with final project

--- main.js
+++ main.js
@@ -13,114 +13,186 @@ for (let i = 0; i < items.length; i++) {
   element.style.background = '#eee';
 }
 
-// const firstItem = document.querySelector('li');
-// console.log(firstItem);
+// // const firstItem = document.querySelector('li');
+// // console.log(firstItem);
 
-// const list = document.querySelector('ul');
-// console.log(list.children);
+// // const list = document.querySelector('ul');
+// // console.log(list.children);
 
-// for (let i = 0; i < list.children.length; i++) {
-//   // console.log(list.children[i]);
-// }
+// // for (let i = 0; i < list.children.length; i++) {
+// //   // console.log(list.children[i]);
+// // }
 
-// console.log(list.firstElementChild);
-// console.log(list.lastElementChild);
+// // console.log(list.firstElementChild);
+// // console.log(list.lastElementChild);
 
-// parent
-// const element = document.querySelector('li');
-// // console.log(element.parentElement);
+// // parent
+// // const element = document.querySelector('li');
+// // // console.log(element.parentElement);
 
-// // sibling
-// // console.log(element.nextElementSibling);
-// // console.log(element.nextElementSibling.nextElementSibling);
+// // // sibling
+// // // console.log(element.nextElementSibling);
+// // // console.log(element.nextElementSibling.nextElementSibling);
 
-// const lastElement = document.querySelector('#last-course');
-// // console.log(lastElement.previousElementSibling);
+// // const lastElement = document.querySelector('#last-course');
+// // // console.log(lastElement.previousElementSibling);
 
-// // nodes
-// console.log(element.childElementCount); // only count HTML tags
-// console.log(element.childNodes);
+// // // nodes
+// // console.log(element.childElementCount); // only count HTML tags
+// // console.log(element.childNodes);
 
-// // attributes
-// console.log(description.innerHTML);
+// // // attributes
+// // console.log(description.innerHTML);
 
-// event listeners
-// const btn = document.querySelector('.btn');
-// // btn.addEventListener('click', e => {
-// //   console.log(e.target);
+// // event listeners
+// // const btn = document.querySelector('.btn');
+// // // btn.addEventListener('click', e => {
+// // //   console.log(e.target);
+// // // });
+
+// // addGlobalListener('click', btn);
+
+// // function addGlobalListener(eventType, element) {
+// //   document.addEventListener(eventType, e => {
+// //     if (e.target != element) return;
+// //     console.log(element);
+// //   });
+// // }
+
+// // btn.addEventListener('mouseenter', function (e) {
+// //   this.className = 'btn btn-danger';
 // // });
 
-// addGlobalListener('click', btn);
+// // btn.addEventListener('mouseout', function (e) {
+// //   this.className = 'btn btn-primary';
+// // });
 
-// function addGlobalListener(eventType, element) {
-//   document.addEventListener(eventType, e => {
-//     if (e.target != element) return;
-//     console.log(element);
-//   });
+// // setTimeout(
+// //   (p1, p2, p3) => {
+// //     console.log(p1, p2, p3);
+// //   },
+// //   3000,
+// //   'argumento 1',
+// //   'argumento 2',
+// //   'argumento 3'
+// // );
+
+// // const input = document.getElementById('input');
+// // input.addEventListener('keydown', e => {
+// //   console.log(`Tecla presionada: ${e.key}`);
+// // });
+
+// const form = document.getElementById('course-form');
+// form.addEventListener('submit', e => {
+//   e.preventDefault();
+
+//   const title = document.getElementById('title-form').value;
+//   const description = document.getElementById('description-form').value;
+
+//   console.log(title, description);
+// });
+
+// const checkbox = document.getElementById('checkbox');
+// checkbox.addEventListener('change', e => {
+//   console.log('cmabio de valor');
+// });
+
+// const title_form = document.getElementById('title-form');
+
+// title_form.addEventListener('change', e => {
+//   console.log('cmabio de valor');
+// });
+
+// const element = document.querySelector('li');
+// const list = document.querySelector('ul');
+// const div_row = document.querySelector('.row');
+// const div_container = document.querySelector('.container');
+// const body = document.body;
+
+// element.addEventListener('click', show_message);
+
+// list.addEventListener('click', show_message);
+
+// div_row.addEventListener('click', show_message);
+
+// div_container.addEventListener('click', show_message);
+
+// body.addEventListener('click', show_message);
+
+// function show_message(e) {
+//   e.stopPropagation();
+//   console.log('Target: ' + e.target.tagName);
+//   console.log('Current element: ' + this.tagName);
 // }
 
-// btn.addEventListener('mouseenter', function (e) {
-//   this.className = 'btn btn-danger';
-// });
+function createCardWithInnerHTML(title, description) {
+  const html = `<div class="col-sm-6 col-md-4">
+                  <div class="thumbnail">
+                    <div class="caption">
+                      <h3 id="title-card">${title}</h3>
+                      <p id="description-card">${description}</p>
+                      <p>
+                        <a href="" class="btn btn-danger">Accion</a>
+                      </p>
+                    </div>
+                  </div>
+                </div>`;
 
-// btn.addEventListener('mouseout', function (e) {
-//   this.className = 'btn btn-primary';
-// });
+  row.innerHTML += html;
+}
 
-// setTimeout(
-//   (p1, p2, p3) => {
-//     console.log(p1, p2, p3);
-//   },
-//   3000,
-//   'argumento 1',
-//   'argumento 2',
-//   'argumento 3'
-// );
+function createCard(title, description) {
+  let cardWrapper = document.createElement('div');
+  cardWrapper.id = 'card-wrapper';
+  cardWrapper.className = 'col-sm-6 col-md-4';
 
-// const input = document.getElementById('input');
-// input.addEventListener('keydown', e => {
-//   console.log(`Tecla presionada: ${e.key}`);
-// });
+  let thumbnail = document.createElement('div');
+  thumbnail.className = 'thumbnail';
+
+  let caption = document.createElement('div');
+  caption.className = 'caption';
+
+  let cardTitle = document.createElement('h3');
+  cardTitle.textContent = title;
+
+  let cardDescription = document.createElement('p');
+  cardDescription.textContent = description;
+
+  let ctaWrapper = document.createElement('p');
+  let btn = document.createElement('a');
+  btn.href = '#';
+  btn.className = 'btn btn-danger';
+  btn.textContent = 'Eliminar';
+
+  ctaWrapper.addEventListener('click', deleteCard);
+
+  ctaWrapper.appendChild(btn);
+
+  caption.appendChild(cardTitle);
+  caption.appendChild(cardDescription);
+  caption.appendChild(ctaWrapper);
+
+  thumbnail.appendChild(caption);
+
+  cardWrapper.appendChild(thumbnail);
+
+  row.appendChild(cardWrapper);
+}
+
+function deleteCard(e) {
+  e.target.closest('#card-wrapper').remove();
+}
+
+const row = document.querySelector('.row');
 
 const form = document.getElementById('course-form');
+
 form.addEventListener('submit', e => {
   e.preventDefault();
+  const titleElement = document.getElementById('title-form');
+  const descriptionElement = document.getElementById('description-form');
 
-  const title = document.getElementById('title-form').value;
-  const description = document.getElementById('description-form').value;
-
-  console.log(title, description);
+  createCard(titleElement.value, descriptionElement.value);
+  titleElement.value = '';
+  descriptionElement.value = '';
 });
-
-const checkbox = document.getElementById('checkbox');
-checkbox.addEventListener('change', e => {
-  console.log('cmabio de valor');
-});
-
-const title_form = document.getElementById('title-form');
-
-title_form.addEventListener('change', e => {
-  console.log('cmabio de valor');
-});
-
-const element = document.querySelector('li');
-const list = document.querySelector('ul');
-const div_row = document.querySelector('.row');
-const div_container = document.querySelector('.container');
-const body = document.body;
-
-element.addEventListener('click', show_message);
-
-list.addEventListener('click', show_message);
-
-div_row.addEventListener('click', show_message);
-
-div_container.addEventListener('click', show_message);
-
-body.addEventListener('click', show_message);
-
-function show_message(e) {
-  e.stopPropagation();
-  console.log('Target: ' + e.target.tagName);
-  console.log('Current element: ' + this.tagName);
-}
